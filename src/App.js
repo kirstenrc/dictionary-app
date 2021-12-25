15cce4d3511b25ac2a3f451c09534bc8603c68ed
kirstenrc <86810764+kirstenrc@users.noreply.github.com>
2021-12-25 11:07:39 -0800

    Create-react-app, set up file structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Dictionary from './Dictionary';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Dictionary</h1>
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>Coded by Kirsten Cox</footer>
+    </div>
+  );
+}
+
+export default App;
