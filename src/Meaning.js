64511b065e1e7d54151d4996e87b15747370d594
kirstenrc <86810764+kirstenrc@users.noreply.github.com>
2021-12-26 10:56:21 -0800

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,22 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
+        console.log(definition);
         return (
           <div key={index}>
-            <p>
+            <div>
               {definition.definition}
               <br />
               <em>{definition.example}</em>
-            </p>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
+            </div>
           </div>
         );
       })}
